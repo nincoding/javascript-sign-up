b5525a5ed075e9850beb4d0e74050f2e52baaf50
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-01-06 13:14:01 +0900

    feat: 생년월일 입력시 이동 효과추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
+const init = document.querySelector('body');
 const sign = document.querySelector('.app');
 const transitionButton = sign.querySelector('.transitionButton');
 const memberButton =  sign.querySelector('.memberButton');
+const input = sign.querySelector('.memberNumberFormatInputBox');
+const ageMessage = document.querySelector('.memberNumberFormatLabel');
 
-transitionButton.addEventListener('click', function (event) {
+
+
+init.addEventListener('click', function (event) {
   const target = event.target;
   const action = target.classList[0];
-  console.log('a');
   console.log(action)
- 
+
+  if(target.matches('input')) {
+    if(action === 'memberNumberFormatInputBox') {
+      changeStyle();
+      console.log(target);
+    }
+  }
+
+  if(!target.matches('input')) {
+    initStyle();
+  }
 })
+
+function changeStyle() {
+  document.querySelector('.age').style.borderBottom = '1px solid #1ea1f7';
+  input.value = '____/__/__';
+  input.setSelectionRange(0,0);
+
+  ageMessage.style.transition = 'all 150ms linear';
+  ageMessage.style.top = '300px';
+  ageMessage.style.fontSize = '14px';
+  ageMessage.style.color = '#1ea1f7';
+}
+
+function initStyle() {
+  document.querySelector('.age').style.borderBottom = '1px solid #dddfe4';
+  document.querySelector('.memberNumberFormatLabel').style.top = '';
+  ageMessage.style.fontSize = '17px';
+  ageMessage.style.color = '#acabb4';
+  input.value = '';
+}
 
 memberButton.addEventListener('click', function (event) {
   const target = event.target;
-  const action = target.classList[0];
   console.log('b');
-  console.log(action)
- 
 })
 
 //console.log('a');
